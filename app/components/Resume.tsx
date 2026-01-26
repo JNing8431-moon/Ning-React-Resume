8ba9dcd4a896c2341cd4aae2bf10227771d1e9cc
JNing8431 <tanjn-wm22@student.tarc.edu.my>
2026-01-26 15:05:52 +0800

    update personal picture

--- app/components/Resume.tsx
+++ app/components/Resume.tsx
@@ -35,7 +35,7 @@ export default function Resume() {
                     <div className="relative w-32 h-32 md:w-40 md:h-40 shrink-0">
                         <div className="absolute inset-0 bg-teal-500 blur-2xl opacity-20 rounded-full"></div>
                         <Image
-                            src="/Ning-React-Resume/photo.jpg"
+                            src="/Ning-React-Resume/public/photo.jpg"
                             alt="Profile"
                             width={160}
                             height={160}
